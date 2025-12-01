aab030d1e5f91b34ec0e321a0d02bd0b82e97ee1
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-12-01 22:49:34 +0900

    chore(github): PR 및 Issue 템플릿 추가

---
+++ src/main/java/com/lxp/aplus/APlusApplication.java
@@ -0,0 +1,13 @@
+package com.lxp.aplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class APlusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(APlusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lxp/aplus/APlusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxp.aplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class APlusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
