1288ebe62e70fbd9e028674c4ab989a9fcccdd35
juyeongMoon888 <whoor2828@gmail.com>
2025-12-06 21:52:36 +0900

    fix: Lecture 엔티티에 public 누락 수정

--- src/main/java/com/lxp/aplus/course/domain/Lecture.java
+++ src/main/java/com/lxp/aplus/course/domain/Lecture.java
@@ -11,7 +11,7 @@ import java.util.List;
 @Entity
 @Table(name = "lectures")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-class Lecture extends BaseTimeEntity {
+public class Lecture extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
