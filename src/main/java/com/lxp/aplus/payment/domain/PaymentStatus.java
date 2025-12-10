595af959bfa00202a074e906908fad00f5a93a4d
dawwson <shines1427@gmail.com>
2025-12-10 12:50:34 +0900

    refactor(payment): enum에 description 추가

--- src/main/java/com/lxp/aplus/payment/domain/PaymentStatus.java
+++ src/main/java/com/lxp/aplus/payment/domain/PaymentStatus.java
@@ -1,14 +1,16 @@
 package com.lxp.aplus.payment.domain;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum PaymentStatus {
-    // 결제 대기
-    PENDING,
-    // 결제 승인 성공
-    APPROVED,
-    // 결제 승인 실패
-    FAILED,
-    // 결제 승인 취소
-    CANCELED,
-    // 결제 환불
-    REFUNDED
+    PENDING("결제 대기"),
+    APPROVED("결제 승인"),
+    FAILED("결제 승인"),
+    CANCELED("결제 취소"),
+    REFUNDED("결제 환불");
+
+    private final String description;
 }
