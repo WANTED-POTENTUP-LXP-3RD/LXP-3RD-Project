f5e4dd46b1d676bd37d4cedfa950097893fd9c0b
dawwson <shines1427@gmail.com>
2025-12-10 21:41:56 +0900

    comment(order): TODO 작성

--- src/main/java/com/lxp/aplus/order/domain/Order.java
+++ src/main/java/com/lxp/aplus/order/domain/Order.java
@@ -15,6 +15,7 @@ import java.util.UUID;
 
 // TODO: amount, currency 묶어서 VO(Money)로 만들기
 // TODO: 특정 상태에 종속적인 필드를 관리하는 구조적인 방법 고민(approvedPaymentId, cancelReason)
+// TODO: custom exception 적용하기
 @Entity
 @Table(name = "orders")
 @Getter
