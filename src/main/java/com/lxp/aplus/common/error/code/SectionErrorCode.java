e42929591e5bb40a94a39ccab0c209f041d34496
Donghoon Jeong <112836685+jjeongdong@users.noreply.github.com>
2025-12-10 17:35:37 +0900

    fix(course): SectionErrorCode 수정

--- src/main/java/com/lxp/aplus/common/error/code/SectionErrorCode.java
+++ src/main/java/com/lxp/aplus/common/error/code/SectionErrorCode.java
@@ -12,7 +12,6 @@ public enum SectionErrorCode implements ErrorCode {
     SECTION_ACCESS_DENIED(HttpStatus.FORBIDDEN, "ES002", "해당 섹션에 대한 접근 권한이 없습니다."),
     SECTION_ORDER_DUPLICATED(HttpStatus.BAD_REQUEST, "ES003", "이미 존재하는 섹션 순서입니다.");
 
-
     private final HttpStatus status;
     private final String code;
     private final String message;
