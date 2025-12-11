e2024be71ce25d6e809964a6a04d8b4d287d92de
dawwson <shines1427@gmail.com>
2025-12-11 13:10:23 +0900

    comment(payment): TODO 주석 추가

--- src/main/java/com/lxp/aplus/order/application/usecase/OrderCommandUseCase.java
+++ src/main/java/com/lxp/aplus/order/application/usecase/OrderCommandUseCase.java
@@ -27,7 +27,6 @@ public class OrderCommandUseCase {
     public OrderCreateResult createOrderFromCourseIds(Long userId, List<Long> courseIds) {
 
         // 1. Course 가격 조회 (From Course BC)
-        // TODO: 이벤트로 수정
         List<CoursePrice> coursePrices = getCoursePriceByIds(courseIds);
 
         // 2. OrderLine 리스트 생성
@@ -42,12 +41,13 @@ public class OrderCommandUseCase {
         return OrderCreateResult.of(order.getOrderId(), order.getAmount());
     }
 
-    // TODO: 이벤트 기반으로 분리 ----------
+    // TODO: 포트/어댑터 패턴으로 필요한 DTO로 변환하기 ----------
     // NOTE: Course의 코드를 최대한 덜 건드리기 위해서 이곳에 임시로 작성한 코드입니다.
     private List<CoursePrice> getCoursePriceByIds(List<Long> ids) {
         List<Course> courses = courseRepository.findByIdIn(ids);
 
         if (courses.size() != ids.size()) {
+            // TODO: 정확히 어떤 courseId가 없는지도 전달할 필요성 고민!
             throw new BusinessException(CourseErrorCode.COURSE_NOT_FOUND);
         }
 
@@ -55,5 +55,5 @@ public class OrderCommandUseCase {
                 .map(course -> new CoursePrice(course.getId(), course.getPrice()))
                 .collect(Collectors.toList());
     }
-    // --------------------------------
+    // --------------------------------------------------
 }
