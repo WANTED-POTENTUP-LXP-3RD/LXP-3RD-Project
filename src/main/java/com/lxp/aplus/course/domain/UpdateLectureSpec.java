c6ef79378bee5b5873bd67918de629d14e8a3333
Juyeong Moon <whoor2828@gmail.com>
2025-12-11 10:42:25 +0900

    refactor(Lecture): 검증 수정

--- src/main/java/com/lxp/aplus/course/domain/UpdateLectureSpec.java
+++ src/main/java/com/lxp/aplus/course/domain/UpdateLectureSpec.java
@@ -2,7 +2,6 @@ package com.lxp.aplus.course.domain;
 
 import com.lxp.aplus.course.application.command.UpdateLectureCommand;
 import lombok.Builder;
-import org.hibernate.sql.Update;
 
 @Builder
 public record UpdateLectureSpec(
