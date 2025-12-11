b97b4adf9e4bc5a0619791fdd9ed924b6a716487
Juyeong Moon <whoor2828@gmail.com>
2025-12-11 11:41:15 +0900

    feat(LEcture): 강의 삭제 성공 코드 추가

--- src/main/java/com/lxp/aplus/common/result/code/LectureResultCode.java
+++ src/main/java/com/lxp/aplus/common/result/code/LectureResultCode.java
@@ -11,7 +11,7 @@ public enum LectureResultCode implements ResultCode {
     LECTURE_REGISTER_SUCCESS(HttpStatus.CREATED, "SL001", "강의가 정상적으로 등록되었습니다."),
     LECTURE_UPDATE_SUCCESS(HttpStatus.OK, "SL002", "강의 정보가 수정되었습니다."),
     LECTURE_READ_SUCCESS(HttpStatus.OK, "SL003", "강의 정보에 성공했습니다."),
-    LECTURE_DELETE_SUCCESS(HttpStatus.OK, "SL004", "강의가 정상적으로 삭제외었습니다.");
+    LECTURE_DELETE_SUCCESS(HttpStatus.OK, "SL004", "강의가 정상적으로 삭제되었습니다.");
 
     private final HttpStatus status;
     private final String code;
