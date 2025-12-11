08477c53adbec7442c68cd81d67a649c3dce182d
Juyeong Moon <whoor2828@gmail.com>
2025-12-11 10:42:46 +0900

    refactor(Lecture): 검증 수정

--- src/main/java/com/lxp/aplus/course/presentation/request/LectureUpdateRequest.java
+++ src/main/java/com/lxp/aplus/course/presentation/request/LectureUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.lxp.aplus.course.presentation.request;
 
 import com.lxp.aplus.course.application.command.UpdateLectureCommand;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record LectureUpdateRequest(
@@ -12,6 +13,9 @@ public record LectureUpdateRequest(
 
         @NotNull(message = "미리보기 여부는 필수입니다.")
         Boolean isPreview,
+
+        @Min(value = 1, message = "순서 인덱스는 1 이상이어야 합니다.")
+        int orderIndex,
 
         @NotNull(message = "강의 자료는 필수입니다.")
         LectureResourceRequest resource
