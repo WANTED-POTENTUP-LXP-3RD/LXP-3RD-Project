a25c0652851177c8636a58273636a604404c3829
Juyeong Moon <whoor2828@gmail.com>
2025-12-11 12:25:47 +0900

    feat(Lecture): 커스텀 오류 코드 LECTURE_RESOURCE_REQUIRED 추가

--- src/main/java/com/lxp/aplus/common/error/code/LectureErrorCode.java
+++ src/main/java/com/lxp/aplus/common/error/code/LectureErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum LectureErrorCode implements ErrorCode {
     LECTURE_NOT_FOUND(HttpStatus.NOT_FOUND, "EL001", "해당 강의를 찾을 수 없습니다."),
-    LECTURE_CREATE_FAILED(HttpStatus.BAD_REQUEST, "EL002", "강의 생성에 실패했습니다.");
+    LECTURE_CREATE_FAILED(HttpStatus.BAD_REQUEST, "EL002", "강의 생성에 실패했습니다."),
+    LECTURE_RESOURCE_REQUIRED(HttpStatus.BAD_REQUEST, "EL006", "강의 자료는 필수 항목입니다.");
 
     private final HttpStatus status;
     private final String code;
