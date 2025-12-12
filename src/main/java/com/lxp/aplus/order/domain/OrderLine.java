9b8634f9d4f4238257926590818e5bec7f72c473
dawwson <shines1427@gmail.com>
2025-12-12 16:44:39 +0900

    feat(order): itemType enum string으로 적용

--- src/main/java/com/lxp/aplus/order/domain/OrderLine.java
+++ src/main/java/com/lxp/aplus/order/domain/OrderLine.java
@@ -1,8 +1,9 @@
 package com.lxp.aplus.order.domain;
 
 import com.lxp.aplus.order.application.CoursePrice;
-import com.lxp.aplus.order.application.usecase.OrderCommandUseCase;
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -41,7 +42,9 @@ import java.math.BigDecimal;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderLine {
 
+    @Enumerated(EnumType.STRING)
     private ItemType itemType;
+
     private Long itemId;
     private BigDecimal price;
 
