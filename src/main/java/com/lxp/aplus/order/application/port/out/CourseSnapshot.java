aebf5e25246548afd601d60302345fd08a62d7ac
dawwson <shines1427@gmail.com>
2026-01-15 20:34:13 +0900

    fix(cart): getThumbnailUrl -> getThumbnailResourceKey로 변경

--- src/main/java/com/lxp/aplus/order/application/port/out/CourseSnapshot.java
+++ src/main/java/com/lxp/aplus/order/application/port/out/CourseSnapshot.java
@@ -22,7 +22,7 @@ public record CourseSnapshot(
                 .courseTitle(course.getTitle())
                 .courseStatus(course.getCourseStatus())
                 .instructorName(instructor.getName())
-                .thumbnailUrl(course.getThumbnailUrl())
+                .thumbnailUrl(course.getThumbnailResourceKey())
                 .price(course.getPrice())
                 .build();
     }
