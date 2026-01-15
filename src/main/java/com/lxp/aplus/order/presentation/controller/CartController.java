be8006a15fb9b531f0240967ca5ec70186d35f9b
dawwson <shines1427@gmail.com>
2026-01-15 17:38:14 +0900

    feat(cart): controller 구현

--- src/main/java/com/lxp/aplus/order/presentation/controller/CartController.java
+++ src/main/java/com/lxp/aplus/order/presentation/controller/CartController.java
@@ -4,16 +4,17 @@ import com.lxp.aplus.common.result.ResultResponse;
 import com.lxp.aplus.common.security.Authenticated;
 import com.lxp.aplus.order.application.command.CartRemoveItemCommand;
 import com.lxp.aplus.order.application.usecase.CartCommandUseCase;
+import com.lxp.aplus.order.application.usecase.CartQueryUseCase;
 import com.lxp.aplus.order.presentation.request.CartAddItemRequest;
 import com.lxp.aplus.order.presentation.response.CartAddItemResponse;
+import com.lxp.aplus.order.presentation.response.CartGetItemsResponse;
 import com.lxp.aplus.order.presentation.response.CartRemoveItemResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static com.lxp.aplus.common.result.code.CartResultCode.CART_ADD_ITEM_SUCCESS;
-import static com.lxp.aplus.common.result.code.CartResultCode.CART_REMOVE_ITEM_SUCCESS;
+import static com.lxp.aplus.common.result.code.CartResultCode.*;
 
 @RestController
 @RequestMapping("/api/cart")
@@ -21,6 +22,19 @@ import static com.lxp.aplus.common.result.code.CartResultCode.CART_REMOVE_ITEM_S
 public class CartController {
 
     private final CartCommandUseCase cartCommandUseCase;
+    private final CartQueryUseCase cartQueryUseCase;
+
+    @GetMapping
+    public ResponseEntity<ResultResponse<CartGetItemsResponse>> getCartAllItems(
+            @Authenticated Long userId
+    ) {
+
+        CartGetItemsResponse response = cartQueryUseCase.getCartItems(userId);
+
+        return ResponseEntity
+                .status(CART_GET_ITEMS_SUCCESS.getStatus())
+                .body(ResultResponse.of(CART_GET_ITEMS_SUCCESS, response));
+    }
 
     @PostMapping("/items")
     public ResponseEntity<ResultResponse<CartAddItemResponse>> addCartItem(
