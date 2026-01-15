66b973ae9f501e8b2545cd3957a4055d4f1ff2fb
dawwson <shines1427@gmail.com>
2026-01-15 17:41:23 +0900

    feat(cart): course 도메인 접근 port & adapter 작성

--- src/main/java/com/lxp/aplus/order/application/port/out/CourseQueryPort.java
+++ src/main/java/com/lxp/aplus/order/application/port/out/CourseQueryPort.java
@@ -14,4 +14,9 @@ public interface CourseQueryPort {
      * course별 판매 상태(가격, 상태) 조회
      */
     Map<Long, CourseSalesStatus> getCourseSalesStatusByIds(List<Long> courseIds);
+
+    /*
+     * course의 구매 시점의 상품 정보 조회
+     */
+    Map<Long, CourseSnapshot> getCourseSnapshot(List<Long> courseIds);
 }
