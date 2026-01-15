8c0a1906c5541f49940488fb56d30e8e31e9b427
dawwson <shines1427@gmail.com>
2026-01-15 17:38:54 +0900

    feat(user): user repository - 여러 유저 조회 메서드 추가

--- src/main/java/com/lxp/aplus/user/domain/UserRepository.java
+++ src/main/java/com/lxp/aplus/user/domain/UserRepository.java
@@ -1,10 +1,12 @@
 package com.lxp.aplus.user.domain;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
     User save(User user);
     Optional<User> findById(Long id);
+    List<User> findByIdIn(List<Long> ids);
     Optional<User> findUserWithRolesById(Long id);
     Optional<User> findByEmail(String email);
     Optional<User> findUserWithRolesByEmail(String email);
