aba97fe0b9f240e8ac59738456d323f699aba721
sunwook00 <kckk116560@gmail.com>
2026-02-08 20:44:30 +0900

    refactor(progress): Progress 외부 연동 수정

--- src/main/java/com/lxp/aplus/progress/adapter/out/external/LectureProviderAdapter.java
+++ src/main/java/com/lxp/aplus/progress/adapter/out/external/LectureProviderAdapter.java
@@ -1,7 +1,8 @@
 package com.lxp.aplus.progress.adapter.out.external;
 
+import com.lxp.aplus.common.error.BusinessException;
+import com.lxp.aplus.course.application.port.in.CourseQueryToProgressUseCase;
 import com.lxp.aplus.course.application.port.in.dto.ResourceSummary;
-import com.lxp.aplus.course.infrastructure.persistence.CourseJpaRepository;
 import com.lxp.aplus.progress.application.port.out.LectureProvider;
 import com.lxp.aplus.progress.application.port.out.dto.LectureDurationDto;
 import com.lxp.aplus.progress.application.port.out.dto.LectureSummaryDto;
@@ -15,18 +16,22 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 class LectureProviderAdapter implements LectureProvider {
 
-    private final CourseJpaRepository courseJpaRepository;
+    private final CourseQueryToProgressUseCase courseQueryToProgressUseCase;
 
     @Override
     public LectureDurationDto getLectureInfo(Long lectureResourceId) {
-        return courseJpaRepository.findLectureDurationByResourceId(lectureResourceId)
-                .map(LectureDurationDto::new)
-                .orElse(null);
+        try {
+            return new LectureDurationDto(
+                    courseQueryToProgressUseCase.getLectureDuration(lectureResourceId).totalDurationSeconds()
+            );
+        } catch (BusinessException e) {
+            return null;
+        }
     }
 
     @Override
     public List<LectureSummaryDto> getLectureDetailsByCourseId(Long courseId) {
-        List<ResourceSummary> resourceSummaries = courseJpaRepository.findLectureSummariesByCourseId(courseId);
+        List<ResourceSummary> resourceSummaries = courseQueryToProgressUseCase.findLectureSummariesByCourseId(courseId);
         return resourceSummaries.stream()
                 .map(LectureSummaryDto::from)
                 .collect(Collectors.toList());
