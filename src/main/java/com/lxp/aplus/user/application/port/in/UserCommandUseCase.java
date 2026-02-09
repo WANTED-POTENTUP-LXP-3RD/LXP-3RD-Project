92d1343112eae23e479fc22cabc38e0c717de8c5
sunwook00 <kckk116560@gmail.com>
2026-02-09 11:56:38 +0900

    merge(user) : merge안되는 패키지 + 파일 생성

---
+++ src/main/java/com/lxp/aplus/user/application/port/in/UserCommandUseCase.java
@@ -0,0 +1,25 @@
+package com.lxp.aplus.user.application.port.in;
+
+import com.lxp.aplus.user.application.dto.request.ActivateUserRequest;
+import com.lxp.aplus.user.application.dto.request.ChangePasswordRequest;
+import com.lxp.aplus.user.application.dto.request.CreateUserRequest;
+import com.lxp.aplus.user.application.dto.request.DeleteUserRequest;
+import com.lxp.aplus.user.application.dto.request.UpdateUserInfoRequest;
+import com.lxp.aplus.user.application.dto.request.WithdrawUserRequest;
+import com.lxp.aplus.user.application.dto.response.UserResponse;
+
+public interface UserCommandUseCase {
+    UserResponse createUser(CreateUserRequest request);
+
+    UserResponse updateUserInfo(UpdateUserInfoRequest request);
+
+    UserResponse addInstructorRole(Long userId);
+
+    UserResponse withdrawUser(WithdrawUserRequest request);
+
+    UserResponse activateUser(ActivateUserRequest request);
+
+    void changePassword(ChangePasswordRequest request);
+
+    void deleteUser(DeleteUserRequest request);
+}
