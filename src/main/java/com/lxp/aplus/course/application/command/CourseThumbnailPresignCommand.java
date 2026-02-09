830232dd00a347956ea0f31ad6928a6afb5e7845
hojun-lang <milky9925@gmail.com>
2026-02-09 19:31:04 +0900

    feat(course) : Thumbnail @Valid 검증 메세지 추가

--- src/main/java/com/lxp/aplus/course/application/command/CourseThumbnailPresignCommand.java
+++ src/main/java/com/lxp/aplus/course/application/command/CourseThumbnailPresignCommand.java
@@ -5,10 +5,10 @@ import jakarta.validation.constraints.Size;
 
 public record CourseThumbnailPresignCommand(
 
-        @NotBlank
-        @Size(max = 255)
+        @NotBlank(message = "파일명은 필수입니다.")
+        @Size(max = 255, message = "파일명은 255자를 넘어갈 수 없습니다.")
         String originalFileName,
 
-        @NotBlank
+        @NotBlank(message = "컨텐츠 타입은 필수입니다.")
         String contentType
 ) {}
